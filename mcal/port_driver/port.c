13dcec19eacde68d15a99745df8e1f676452ac94
--replace-all <--replace-all>
2022-08-26 16:53:35 +0200

    GPIO vid

--- mcal/port_driver/port.c
+++ mcal/port_driver/port.c
@@ -13,8 +13,8 @@ void port_init(void)
     Port_Type PortId;
     Port_ChIdType ChId;
     volatile PORT_RegType* PORT_Addr;
-	
-	
+
+
     for (i = 0; i < PORT_NUM_OF_ACTIVATED_CH; ++i)
     {
         PortId = i/ MAX_NUM_OF_CH_IN_PORT;
@@ -45,63 +45,62 @@ void port_init(void)
         /* set channel direction */
         if(PortCfgArr[i].Dir == Dir_Out)
         {
-            
+
 					PORT_Addr->GPIODIR |= ((uint32)1<<ChId);
         }
         else
         {
-            
+
 					PORT_Addr->GPIODIR &= ~((uint32)1<<ChId);
         }
 
         /* set channel mode */
         if(PortCfgArr[i].Mode == PIN_DIO_M)
         {
-            
+
 					PORT_Addr->GPIOAFSEL &= ~((uint32)1<<ChId);
         }
         else
         {
-         
-					PORT_Addr->GPIOAFSEL |= ((uint32)1<<ChId);
 
-            PORT_Addr->GPIOPCTL |= ( PortCfgArr[i].Mode << (ChId*4) );
+					PORT_Addr->GPIOAFSEL |= ((uint32)1<<ChId);
+          PORT_Addr->GPIOPCTL |= ( PortCfgArr[i].Mode << (ChId*4) );
         }
 
- 
+
         /* set Internal Attachment configuration */
         if(PortCfgArr[i].InterfaceResType == PullUp)
         {
-            
+
 					PORT_Addr->GPIOPUR |= ((uint32)1<<ChId);
         }else if(PortCfgArr[i].InterfaceResType == PullDown)
         {
-            
+
 					PORT_Addr->GPIOPDR |= ((uint32)1<<ChId);
         }else if(PortCfgArr[i].InterfaceResType == OpenDrain)
         {
-           
+
 					PORT_Addr->GPIOODR |= ((uint32)1<<ChId);
         }
 
         /*set current strength configuration */
         if(PortCfgArr[i].CurrentDrive == Curr_2mA)
         {
-            
+
 					PORT_Addr->GPIODR2R |= ((uint32)1<<ChId);
         }else if(PortCfgArr[i].CurrentDrive == Curr_4mA)
         {
-            
+
 					PORT_Addr->GPIODR4R |= ((uint32)1<<ChId);
         }else if(PortCfgArr[i].CurrentDrive == Curr_8mA)
         {
-            
+
 					PORT_Addr->GPIODR8R |= ((uint32)1<<ChId);
         }
-				
-				
+
+
 				PORT_Addr->GPIODEN |= ((uint32)1<<ChId);
 
-				
+
     }
 }
