cf248670189e8c3362a38a503d6d8e5e8492a1f7
--replace-all <--replace-all>
2022-08-31 23:03:02 +0200

    tme on off define

--- appl/main.c
+++ appl/main.c
@@ -4,15 +4,27 @@
 #include "Gpio.h"
 #include "SysTick_cfg.h"
 
+
+uint8 flage=0;
+uint8 counter;
+
+/**
+user define
+
+*/
+uint8 onTime=5;   // on time
+uint8 offTime=2;  // off time
+
 int main()
 {
-	
-	
+
+
     SysCtr_init();
     port_init();
+		counter=offTime;
 	  System_Tick_ms(1000);  //ms (1:1000)
 
-	
+
     while(1)
     {
 
@@ -22,9 +34,25 @@ int main()
 
 void SysTick_Handler(void)
 {
-   
-	Gpio_FlipChannel( PF2);
-  
+
+	//Gpio_FlipChannel( PF2);
+	counter--;
+	if(flage==1){
+
+	    if(counter==0){
+				counter=offTime-1;
+			  Gpio_WriteChannel(PF2, STD_low);
+				flage=0;
+			}
+
+	}else{
+	if(counter==0){
+				counter=onTime-1;
+			  Gpio_WriteChannel(PF2, STD_high);
+				flage=1;
+			}
+
+
+	}
+
 }
-
-
